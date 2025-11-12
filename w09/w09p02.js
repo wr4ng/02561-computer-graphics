ccd5b6311c1583e68bcbdbdc6fae936021896f05
Mads Wrang <madscwn@gmail.com>
2025-11-12 10:39:48 +0100

    w09p02

---
+++ w09/w09p02.js
@@ -0,0 +1,499 @@
+"use strict";
+
+function setupInputListeners(onchange) {
+    document.getElementById('emitted-radiance').oninput = onchange;
+    document.getElementById('ambient-radiance').oninput = onchange;
+    document.getElementById('diffuse').oninput = onchange;
+    document.getElementById('specular').oninput = onchange;
+    document.getElementById('shininess').oninput = onchange;
+}
+
+function getOptions() {
+    const emittedRadianceSlider = document.getElementById('emitted-radiance');
+    const ambientRadianceSlider = document.getElementById('ambient-radiance');
+    const diffuseSlider = document.getElementById('diffuse');
+    const specularSlider = document.getElementById('specular');
+    const shininessSlider = document.getElementById('shininess');
+
+    return {
+        emittedRadianceSlider: parseFloat(emittedRadianceSlider.value),
+        ambientRadianceSlider: parseFloat(ambientRadianceSlider.value),
+        diffuseSlider: parseFloat(diffuseSlider.value),
+        specularSlider: parseFloat(specularSlider.value),
+        shininessSlider: parseFloat(shininessSlider.value),
+    };
+}
+
+async function main() {
+    const gpu = navigator.gpu;
+    const adapter = await gpu.requestAdapter();
+    const device = await adapter.requestDevice();
+    const canvas = document.getElementById('my-canvas');
+    const context = canvas.getContext('webgpu');
+    const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
+    context.configure({
+        device: device,
+        format: canvasFormat,
+    });
+
+    // SHARED
+    const positionBufferLayout = {
+        arrayStride: sizeof['vec4'],
+        attributes: [{
+            format: 'float32x4',
+            offset: 0,
+            shaderLocation: 0,
+        }],
+    };
+
+
+    // TEAPOT
+    const obj_filename = "teapot.obj";
+    const obj = await readOBJFile(obj_filename, 1, true);
+
+    const teapotPositions = obj.vertices;
+    const teapotPositionBuffer = device.createBuffer({
+        size: sizeof['vec4'] * teapotPositions.length,
+        usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+    });
+    device.queue.writeBuffer(teapotPositionBuffer, 0, flatten(teapotPositions));
+
+    const teapotColors = obj.colors;
+    const teapotColorBuffer = device.createBuffer({
+        size: sizeof['vec4'] * teapotColors.length,
+        usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+    });
+    const teapotColorBufferLayout = {
+        arrayStride: sizeof['vec4'],
+        attributes: [{
+            format: 'float32x4',
+            offset: 0,
+            shaderLocation: 1,
+        }],
+    };
+    device.queue.writeBuffer(teapotColorBuffer, 0, flatten(teapotColors));
+
+    const teapotNormals = obj.normals;
+    const teapotNormalBuffer = device.createBuffer({
+        size: sizeof['vec4'] * teapotNormals.length,
+        usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+    });
+    const teapotNormalBufferLayout = {
+        arrayStride: sizeof['vec4'],
+        attributes: [{
+            format: 'float32x4',
+            offset: 0,
+            shaderLocation: 2,
+        }],
+    };
+    device.queue.writeBuffer(teapotNormalBuffer, 0, flatten(teapotNormals));
+
+    const teapotIndices = obj.indices;
+    const teapotIndicesBuffer = device.createBuffer({
+        size: sizeof['vec4'] * teapotIndices.length,
+        usage: GPUBufferUsage.INDEX | GPUBufferUsage.COPY_DST,
+    });
+    device.queue.writeBuffer(teapotIndicesBuffer, 0, teapotIndices);
+
+    const teapotUniformBuffer = device.createBuffer({
+        size: sizeof['mat4'] * 2 + sizeof['vec4'] * 4,
+        usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+    });
+
+    // GROUND
+    let positionsGround = [
+        vec3(-2, -1, -1),
+        vec3(2, -1, -1),
+        vec3(2, -1, -5),
+        vec3(-2, -1, -5),
+    ];
+
+    let indicesGround = new Uint32Array([
+        0, 1, 2,
+        0, 2, 3,
+    ]);
+
+
+    const groundPositionBuffer = device.createBuffer({
+        size: sizeof['vec3'] * positionsGround.length,
+        usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+    });
+
+    const groundPositionBufferLayout = {
+        arrayStride: sizeof['vec3'],
+        attributes: [{
+            format: 'float32x3',
+            offset: 0,
+            shaderLocation: 0,
+        }],
+    };
+
+    const groundIndicesBuffer = device.createBuffer({
+        size: sizeof['vec3'] * indicesGround.length,
+        usage: GPUBufferUsage.INDEX | GPUBufferUsage.COPY_DST,
+    });
+
+    const groundTexcoords = [
+        vec2(0.0, 0.0),
+        vec2(1.0, 0.0),
+        vec2(1.0, 1.0),
+        vec2(0.0, 1.0),
+    ];
+    const groundTexcoordBuffer = device.createBuffer({
+        size: sizeof['vec2'] * groundTexcoords.length,
+        usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+    });
+    const groundTexcoordBufferLayout = {
+        arrayStride: sizeof['vec2'],
+        attributes: [{
+            format: 'float32x2',
+            offset: 0,
+            shaderLocation: 3,
+        }],
+    };
+
+    device.queue.writeBuffer(groundPositionBuffer, 0, flatten(positionsGround));
+    device.queue.writeBuffer(groundIndicesBuffer, 0, indicesGround);
+    device.queue.writeBuffer(groundTexcoordBuffer, 0, flatten(groundTexcoords));
+
+    const filename = 'xamp23.png';
+    const response = await fetch(filename);
+    const blob = await response.blob();
+    const img = await createImageBitmap(blob, { colorSpaceConversion: 'none' });
+
+    const groundTexture = device.createTexture({
+        size: [img.width, img.height, 1],
+        format: "rgba8unorm",
+        usage: GPUTextureUsage.COPY_DST | GPUTextureUsage.TEXTURE_BINDING | GPUTextureUsage.RENDER_ATTACHMENT
+    });
+    device.queue.copyExternalImageToTexture(
+        { source: img, flipY: true },
+        { texture: groundTexture },
+        { width: img.width, height: img.height },
+    );
+
+    groundTexture.sampler = device.createSampler({
+        addressModeU: 'clamp-to-edge',
+        addressModeV: 'clamp-to-edge',
+        minFilter: 'linear',
+        magFilter: 'linear',
+    });
+
+    const bgcolor = vec4(0.3921, 0.5843, 0.9294, 1.0) // Cornflower
+
+    const groundUniformBuffer = device.createBuffer({
+        size: sizeof['mat4'] * 2 + sizeof['vec4'] * 4,
+        usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+    });
+
+    const uniformBufferShadow = device.createBuffer({
+        size: sizeof['mat4'] * 2 + sizeof['vec4'] * 4,
+        usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+    });
+
+    const Mst = mat4(
+        1.0, 0.0, 0.0, 0.0,
+        0.0, 1.0, 0.0, 0.0,
+        0.0, 0.0, 0.5, 0.5,
+        0.0, 0.0, 0.0, 1.0
+    )
+
+    const center = translate(0, 0, 0);
+    const M = center;
+
+
+    const fov = 90;
+    let projection = perspective(fov, canvas.width / canvas.height, 1, 20);
+    projection = mult(Mst, projection);
+
+    const V = mat4();
+    const mvp = mult(projection, mult(V, M));
+
+    device.queue.writeBuffer(groundUniformBuffer, 0, flatten(mvp));
+    device.queue.writeBuffer(groundUniformBuffer, sizeof['mat4'] * 2, new Float32Array([0.0, 0.0, 0.0, 1.0])); // eye, visibility
+
+    const wgslfile = document.getElementById('wgsl').src;
+    const wgslcode
+        = await fetch(wgslfile, { cache: "reload" }).then(r => r.text());
+    const wgsl = device.createShaderModule({
+        code: wgslcode
+    });
+
+    const msaaCount = 4;
+
+    const groundPipeline = device.createRenderPipeline({
+        layout: 'auto',
+        vertex: {
+            module: wgsl,
+            entryPoint: 'main_vs_ground',
+            buffers: [groundPositionBufferLayout, groundTexcoordBufferLayout],
+        },
+        fragment: {
+            module: wgsl,
+            entryPoint: 'main_fs_ground',
+            targets: [{ format: canvasFormat }],
+        },
+        primitive: {
+            topology: 'triangle-list',
+            frontFace: 'ccw',
+            cullMode: 'none',
+        },
+        multisample: { count: msaaCount },
+        depthStencil: {
+            depthWriteEnabled: true,
+            depthCompare: 'less',
+            format: 'depth24plus',
+        },
+    });
+
+    const teapotPipeline = device.createRenderPipeline({
+        layout: 'auto',
+        vertex: {
+            module: wgsl,
+            entryPoint: 'main_vs_teapot',
+            buffers: [positionBufferLayout, teapotColorBufferLayout, teapotNormalBufferLayout],
+        },
+        fragment: {
+            module: wgsl,
+            entryPoint: 'main_fs_teapot',
+            targets: [{ format: canvasFormat }],
+        },
+        primitive: {
+            topology: 'triangle-list',
+            frontFace: 'ccw',
+            cullMode: 'none',
+        },
+        multisample: { count: msaaCount },
+        depthStencil: {
+            depthWriteEnabled: true,
+            depthCompare: 'less',
+            format: 'depth24plus',
+        },
+    });
+
+    // Create another pipeline for shadow pass to change depth test
+    const pipelineShadows = device.createRenderPipeline({
+        layout: 'auto',
+        vertex: {
+            module: wgsl,
+            entryPoint: 'main_vs_ground',
+            buffers: [positionBufferLayout, groundTexcoordBufferLayout],
+        },
+        fragment: {
+            module: wgsl,
+            entryPoint: 'main_fs_ground',
+            targets: [{ format: canvasFormat }],
+        },
+        primitive: {
+            topology: 'triangle-list',
+            frontFace: 'ccw',
+            cullMode: 'none', // No culling to see shadows on ground on both sides
+        },
+        multisample: { count: msaaCount },
+        depthStencil: {
+            depthWriteEnabled: true,
+            depthCompare: 'greater',
+            format: 'depth24plus',
+        },
+    });
+
+    const msaaTexture = device.createTexture({
+        size: { width: canvas.width, height: canvas.height },
+        format: canvasFormat,
+        sampleCount: msaaCount,
+        usage: GPUTextureUsage.RENDER_ATTACHMENT,
+    });
+
+    const depthTexture = device.createTexture({
+        size: { width: canvas.width, height: canvas.height },
+        format: 'depth24plus',
+        sampleCount: msaaCount,
+        usage: GPUTextureUsage.RENDER_ATTACHMENT,
+    });
+
+    // Different bind groups for ground and objects to use different textures
+    const groundBindGroup = device.createBindGroup({
+        layout: groundPipeline.getBindGroupLayout(0),
+        entries: [
+            { binding: 0, resource: { buffer: groundUniformBuffer } },
+            { binding: 1, resource: groundTexture.sampler },
+            { binding: 2, resource: groundTexture.createView() },
+        ],
+    });
+
+    const teapotBindGroup = device.createBindGroup({
+        layout: teapotPipeline.getBindGroupLayout(0),
+        entries: [
+            { binding: 0, resource: { buffer: teapotUniformBuffer } },
+        ],
+    });
+
+    const bindGroupShadows = device.createBindGroup({
+        layout: pipelineShadows.getBindGroupLayout(0),
+        entries: [
+            { binding: 0, resource: { buffer: uniformBufferShadow } },
+            { binding: 1, resource: groundTexture.sampler },
+            { binding: 2, resource: groundTexture.createView() },
+        ],
+    });
+
+
+    document.getElementById('toggle-animate-light').onclick = () => {
+        animateLight = !animateLight;
+        if (animateLight && !animateTeapot) {
+            lastTime = performance.now();
+            requestAnimationFrame(animate);
+        }
+    };
+
+    document.getElementById('toggle-animate-teapot').onclick = () => {
+        animateTeapot = !animateTeapot;
+        if (animateTeapot && !animateLight) {
+            lastTime = performance.now();
+            requestAnimationFrame(animate);
+        }
+    };
+
+    let lastTime = performance.now();
+    let animateLight = false;
+    let animateTeapot = false;
+
+    let lightAngle = 45;
+    const lightRadius = 2.0;
+    let lightPos = vec3(lightRadius * Math.sin(lightAngle), 2, -2 + lightRadius * Math.cos(lightAngle));
+
+    function updateLightPosition(timestamp) {
+        // Update light position
+        lightAngle += (timestamp - lastTime) * 0.0025;
+        lightPos = vec3(lightRadius * Math.sin(lightAngle), 2, -2 + lightRadius * Math.cos(lightAngle));
+    }
+
+    let teapotY = -0.5;
+    let direction = 1;
+    let M_teapot = mult(translate(0, teapotY, -3), scalem(0.25, 0.25, 0.25));
+    let mvp_teapot = mult(projection, mult(V, M_teapot));
+    const eye = vec3(0, 0, 0);
+
+    function updateTeatpotPosition(timestamp) {
+        teapotY = teapotY + direction * 0.0005 * (timestamp - lastTime);
+        if (teapotY < -1.0) {
+            teapotY = -1.0;
+            direction = 1;
+        }
+        else if (teapotY > -0.5) {
+            teapotY = -0.5;
+            direction = -1;
+        }
+        M_teapot = mult(translate(0, teapotY, -3), scalem(0.25, 0.25, 0.25));
+        mvp_teapot = mult(projection, mult(V, M_teapot));
+    }
+
+    let mvpShadow = computeShadowMatrix();
+
+    function computeShadowMatrix() {
+        const epsilon = 0.0001;
+        const l = lightPos;
+        const n = vec3(0, 1, 0);
+        const d = 1.0 + epsilon;
+
+        const a = d + dot(n, l);
+        const Mshadow = mat4(
+            a - l[0] * n[0], -l[0] * n[1], -l[0] * n[2], -l[0] * d,
+            -l[1] * n[0], a - l[1] * n[1], -l[1] * n[2], -l[1] * d,
+            -l[2] * n[0], -l[2] * n[1], a - l[2] * n[2], -l[2] * d,
+            -n[0], -n[1], -n[2], a - d
+        );
+
+        const mvpShadow = mult(projection, mult(V, mult(Mshadow, M_teapot)));
+        return mvpShadow;
+    }
+
+    function updateUniforms() {
+        const options = getOptions();
+        const teapotUniforms = new Float32Array([
+            ...flatten(eye), 1.0,
+            ...flatten(lightPos), options.emittedRadianceSlider,
+            options.ambientRadianceSlider, options.diffuseSlider, options.specularSlider, options.shininessSlider,
+        ]);
+        device.queue.writeBuffer(teapotUniformBuffer, 0, flatten(mvp_teapot));
+        device.queue.writeBuffer(teapotUniformBuffer, sizeof['mat4'], flatten(M_teapot));
+        device.queue.writeBuffer(teapotUniformBuffer, sizeof['mat4'] * 2, teapotUniforms);
+
+        device.queue.writeBuffer(uniformBufferShadow, 0, flatten(mvpShadow));
+        device.queue.writeBuffer(uniformBufferShadow, sizeof['mat4'] * 2, new Float32Array([0.0, 0.0, 0.0, 0.0]));
+    }
+
+    function animate(timestamp) {
+        if (animateLight) {
+            updateLightPosition(timestamp);
+        }
+        if (animateTeapot) {
+            updateTeatpotPosition(timestamp);
+        }
+        if (animateLight || animateTeapot) {
+            mvpShadow = computeShadowMatrix();
+        }
+        lastTime = timestamp;
+        updateUniforms();
+        render();
+        if (animateLight || animateTeapot) {
+            requestAnimationFrame(animate);
+        }
+    }
+
+    function render() {
+        console.log("Render frame");
+
+        const encoder = device.createCommandEncoder();
+        const pass = encoder.beginRenderPass({
+            colorAttachments: [{
+                view: msaaTexture.createView(),
+                resolveTarget: context.getCurrentTexture().createView(),
+                loadOp: 'clear',
+                storeOp: 'store',
+                clearValue: { r: bgcolor[0], g: bgcolor[1], b: bgcolor[2], a: bgcolor[3] },
+            }],
+            depthStencilAttachment: {
+                view: depthTexture.createView(),
+                depthLoadOp: "clear",
+                depthClearValue: 1.0,
+                depthStoreOp: "store",
+            }
+        });
+
+        pass.setPipeline(groundPipeline);
+        pass.setIndexBuffer(groundIndicesBuffer, 'uint32');
+        pass.setVertexBuffer(0, groundPositionBuffer);
+        pass.setVertexBuffer(1, groundTexcoordBuffer);
+
+        // Draw ground
+        pass.setBindGroup(0, groundBindGroup);
+        pass.drawIndexed(6);
+
+        // Draw shadows
+        pass.setPipeline(pipelineShadows);
+        pass.setIndexBuffer(teapotIndicesBuffer, 'uint32');
+        pass.setVertexBuffer(0, teapotPositionBuffer);
+        pass.setBindGroup(0, bindGroupShadows);
+        pass.drawIndexed(teapotIndices.length);
+
+        // Draw teapot
+        pass.setPipeline(teapotPipeline);
+        pass.setVertexBuffer(1, teapotColorBuffer);
+        pass.setVertexBuffer(2, teapotNormalBuffer);
+        pass.setBindGroup(0, teapotBindGroup);
+        pass.drawIndexed(teapotIndices.length);
+
+        pass.end();
+        device.queue.submit([encoder.finish()]);
+    }
+
+    setupInputListeners(() => {
+        if (!(animateLight || animateTeapot)) {
+            requestAnimationFrame(animate);
+        }
+    });
+    requestAnimationFrame(animate);
+}
+
+window.onload = function () { main(); }
---
+++ w09/w09p02.wgsl
@@ -0,0 +1,88 @@
+struct Uniforms {
+    mvp: mat4x4f,
+    model: mat4x4f,
+    eye: vec3f,
+    visibility: f32,
+    light_pos: vec3f,
+    L_e: f32,
+    L_a: f32,
+    k_d: f32,
+    k_s: f32,
+    s: f32,
+}
+
+@group(0) @binding(0)
+var<uniform> uniforms: Uniforms;
+@group(0) @binding(1)
+var ourSampler: sampler;
+@group(0) @binding(2)
+var ourTexture: texture_2d<f32>;
+
+// GROUND
+struct VSOutGround {
+    @builtin(position) position: vec4f,
+    @location(0) texCoord: vec2f,
+}
+
+@vertex
+fn main_vs_ground(@location(0) inPos: vec4f, @location(3) texCoord: vec2f, @builtin(instance_index) instance: u32) -> VSOutGround {
+    var vsOut: VSOutGround;
+    vsOut.position = uniforms.mvp * inPos;
+    vsOut.texCoord = texCoord;
+    return vsOut;
+}
+
+@fragment
+fn main_fs_ground(@location(0) texCoords: vec2f) -> @location(0) vec4f {
+    return textureSample(ourTexture, ourSampler, texCoords) * vec4f(uniforms.visibility, uniforms.visibility, uniforms.visibility, 1.0);
+}
+
+// TEAPOT
+struct VSOut {
+    @builtin(position) position: vec4f,
+    @location(0) inPos: vec4f,
+    @location(1) color: vec4f,
+    @location(2) normal: vec4f,
+}
+
+@vertex
+fn main_vs_teapot(@location(0) inPos: vec4f, @location(1) color: vec4f, @location(2) normal: vec4f, @builtin(instance_index) instance: u32) -> VSOut {
+    var vsOut: VSOut;
+    vsOut.position = uniforms.mvp * inPos;
+    vsOut.inPos = inPos;
+    vsOut.color = color;
+    vsOut.normal = normal;
+    return vsOut;
+}
+
+@fragment
+fn main_fs_teapot(@location(0) inPos: vec4f, @location(1) color: vec4f, @location(2) normal: vec4f) -> @location(0) vec4f {
+    let n = normalize(normal.xyz);
+
+    // Fixed diffuse and specular color
+    let k_d = vec3f(1) * uniforms.k_d;
+    let k_a = k_d;
+    let k_s = vec3f(1, 1, 1) * uniforms.k_s;
+
+    // Light direction
+    let l_e = -normalize(uniforms.light_pos - (uniforms.model * inPos).xyz);
+    let omega_i = -l_e;
+    let L_e = vec3f(1, 1, 1) * uniforms.L_e;
+    let L_i = L_e;
+    let L_a = vec3f(1, 1, 1) * uniforms.L_a;
+
+    // Phong reflection model
+    let w_o = normalize(uniforms.eye - inPos.xyz);
+    let w_r = 2 * dot(omega_i, n) * n - omega_i;
+
+    let L_P_rs = k_s * L_i * pow(max(dot(w_r, w_o), 0.0), uniforms.s);
+
+    let L_rd = k_d * L_e * max(dot(n, omega_i), 0.0);
+    let L_ra = k_a * L_a;
+
+    // Modified Phong: avoid specular highlights on the back side. Only show when normal and light angle < 90 deg
+    let L_P_rs_select = select(vec3f(0, 0, 0), L_P_rs, dot(n, omega_i) > 0.0);
+
+    let L_o = L_rd + L_ra + L_P_rs_select;
+    return vec4f(L_o, 1.0);
+}
